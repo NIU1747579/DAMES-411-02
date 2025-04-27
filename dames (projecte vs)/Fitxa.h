f72bdc1130d355b91e221bc540c44fe1983abd9e
NIU1744896 <1744896@uab.cat>
2025-04-27 12:51:03 +0200

    Update Fitxa.h

--- dames (projecte vs)/Fitxa.h
+++ dames (projecte vs)/Fitxa.h
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-#include "Posicio.h"
+//#include "Posicio.h"
 #include "Moviment.h"
 
 typedef enum
@@ -20,13 +20,17 @@ class Fitxa
 {
 public:
 	Fitxa() : tipus_Fitxa(TIPUS_EMPTY) {} //COLOR_FITXA?
-	//void calcularMovimentValids(); //????????????????
+	
+	void calcularMovimentValids(); //????????????????
+
 	void convertirADama();
 	bool esBuida() const { return (tipus_Fitxa == TIPUS_EMPTY); } //per comprovar si la casella esta buida - Per el metode movimentValid
 	bool esContraria(const Fitxa& contrari);
 
 	TipusFitxa getTipus() const { return tipus_Fitxa; }
 	ColorFitxa getColor()const { return color_Fitxa; }
+
+	Moviment getMoviments()const { return moviments; }
 
 	void setTipus(const TipusFitxa& tipus) { tipus_Fitxa = tipus; }
 	void setColor(const ColorFitxa& color) { color_Fitxa = color; }
@@ -35,6 +39,6 @@ public:
 private:
 	TipusFitxa tipus_Fitxa;
 	ColorFitxa color_Fitxa;
-	Moviment movimentValids[100];//?
+	Moviment moviments;//?
 };
 
