51cc047b16c2f5cf9ed111c4fd2fe5ab7664b748
NIU1747579 <marta.tomey@autonoma.cat>
2025-04-29 22:12:54 +0200

    Update Tauler.h

--- dames (projecte vs)/Tauler.h
+++ dames (projecte vs)/Tauler.h
@@ -1,12 +1,12 @@
 #ifndef TAULER_H
 #define TAULER_H
+
 #include <iostream>
+#include <string>
 #include <fstream>
 using namespace std;
 
-//#include "posicio.hpp"
-
-#include "Posicio.h"
+#include "posicio.hpp"
 #include "Fitxa.h"
 #include "Moviment.h"
 
@@ -15,13 +15,13 @@ class Tauler
 public:
 	Tauler() {};
 	void inicialitza(const string& nomFitxer);
-	void actualitzaMovimentsValids() const;
+	void actualitzaMovimentsValids();
 	bool mouFitxa(const Posicio& origen, const Posicio& desti);
 
 	void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]);
 	bool esMovimentValid(int filaOrigen, int colOrigen, int filaDesti, int colDesti);
 	void bufarFitxa(int fila, int columna);
-	string ToString() const; //mostrar en pantalla lestat actual del tauler
+	string toString() const; //mostrar en pantalla lestat actual del tauler
 
 	void obtenirPosicionsPossibles(int fila, int col, Posicio posicions[], int& numPosicions);
 
