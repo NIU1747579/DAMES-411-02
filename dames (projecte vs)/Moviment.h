4433fc6d23a1b8c8ccaa4d8e1fe0b207c52eec90
NIU1747579 <marta.tomey@autonoma.cat>
2025-04-29 22:11:45 +0200

    Update Moviment.h

--- dames (projecte vs)/Moviment.h
+++ dames (projecte vs)/Moviment.h
@@ -1,29 +1,23 @@
+#ifndef MOVIMENT_H  
+#define MOVIMENT_H
+
 #include <iostream>
 using namespace std;
 
-#include "Posicio.h"
+#include "posicio.hpp"
 
 class Moviment
 {
 public:
-	//CADA FITXA TE UNS MOVIMENT VALIDS, ELS MOVIMENTS HAN DE SER DE FITXA
-	Moviment() : m_nCaptures(0),  m_numCaptures(0) {}
+	Moviment() : m_numCaptures(0), m_numPosicions(0) {}
 
 	void setOrigen(const Posicio& o) { m_origen = o; }
 	void setDesti(const Posicio& d) { m_desti = d; }
-	void afegirCaptura(const Posicio& c);
-
-	int getNMoviments() const { return nMovimentsValids; }
-	Moviment getMoviment(int index) const { return m_movimentsValids[index]; }
 
 	Posicio getOrigen() const { return m_origen; }
 	Posicio getDesti() const { return m_desti; }
-	int getNCaptures() const { return m_nCaptures; }
+	int getNCaptures() const { return m_numCaptures; }
 	Posicio getCaptures(int i) { return m_captures[i]; }
-	//metode per compr ovar des de movimentValids, si el mov. que es vol 
-	//bool esMovimentDiagonal() const;
-
-	Posicio getPosicioCaptura() const; //retorna la posicio de la fitxa capturada
 
 	Posicio getUltimaPosicio() const;
 	void afegirCaptura(const Posicio& captura);
@@ -39,10 +33,8 @@ private:
 	Posicio m_captures[12]; //max de captures en un moviment
 	int m_numCaptures;
 
-	Posicio m_posicions[20]; 
-	int m_nPosicions;
-
-	
-
+	Posicio m_posicions[20];
+	int m_numPosicions;
 };
 
+#endif
