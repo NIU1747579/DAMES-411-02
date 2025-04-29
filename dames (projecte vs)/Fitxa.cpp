2f36d7ff8f84541fb2474183189d86df159eb18f
NIU1744896 <1744896@uab.cat>
2025-04-29 18:14:11 +0200

    Update Fitxa.cpp

--- dames (projecte vs)/Fitxa.cpp
+++ dames (projecte vs)/Fitxa.cpp
@@ -10,3 +10,17 @@ bool Fitxa::esContraria(const Fitxa& contrari) const
 	if (esBuida() || contrari.esBuida()) return false;
 	return (color_Fitxa != contrari.color_Fitxa); //si son diferents
 }
+
+void Fitxa::netejaMovimentsValids() {
+	for (int i = 0; i < m_numMoviments; i++) {
+		m_movimentsValids[i] = Moviment();
+	}
+	m_numMoviments = 0;
+}
+
+void Fitxa::afegeixMovimentValid(const Moviment& moviment) {
+	if (m_numMoviments < 20) {
+		m_movimentsValids[m_numMoviments] = moviment;
+		m_numMoviments++;
+	}
+}
