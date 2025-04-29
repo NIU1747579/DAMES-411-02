b71a1eff0726e10581edcb5757095812ec82cf22
NIU1747579 <marta.tomey@autonoma.cat>
2025-04-29 22:12:22 +0200

    Update Posicio.h

--- dames (projecte vs)/Posicio.h
+++ dames (projecte vs)/Posicio.h
@@ -1,29 +1,37 @@
 #include <iostream>
+
+#ifndef POSICIO_H
+#define POSICIO_H
+
+#include <string>
 #include <fstream>
 using namespace std;
 
-int const N_FILES = 8;
-int const N_COLUMNES = 8;
+const int N_FILES = 8;
+const int N_COLUMNES = 8;
 
 class Posicio
 {
 public:
 	Posicio() : m_fila(0), m_columna(0) {}
-	Posicio(int fila, int columna) { m_fila = fila; m_columna = columna; }
-	bool operator==(const Posicio& posicio) const; //comparar dos posicions
+	Posicio(int fila, int columna) { m_fila = fila, m_columna = columna; }
+	Posicio(const string& posicioString);
 
-	bool esValida() const;
-	void fromString(const string& posicio);
-	string toString() const;
 
 	int getFila()const { return m_fila; }
 	int getColumna()const { return m_columna; }
+	bool esValida() const;
+	void fromString(const string& posicio);
+	string toString() const;
+	bool operator==(const Posicio& posicio) const; //comparar dos posicions
 
 private:
 	int m_fila;
 	int m_columna;
 };
 
-ofstream& operator<<(ofstream& fitxer, const Posicio& posicio);
+ostream& operator<<(ostream& out, const Posicio& pos);
+//ofstream& operator<<(ofstream& fitxer, const Posicio& posicio);
 ifstream& operator>>(ifstream& fitxer, Posicio& posicio);
 
+#endif
