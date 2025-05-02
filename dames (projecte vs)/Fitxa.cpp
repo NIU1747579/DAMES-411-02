2d8503159d7de70523204a2278c75c6836556f27
NIU1747579 <marta.tomey@autonoma.cat>
2025-05-02 20:36:21 +0200

    Update Fitxa.cpp

--- dames (projecte vs)/Fitxa.cpp
+++ dames (projecte vs)/Fitxa.cpp
@@ -1,16 +1,5 @@
 #include "Fitxa.h"
 
-void Fitxa::convertirADama()
-{
-	if (tipus_Fitxa == TIPUS_NORMAL)
-		tipus_Fitxa = TIPUS_DAMA;
-}
-bool Fitxa::esContraria(const Fitxa& contrari) const
-{
-	if (esBuida() || contrari.esBuida()) return false;
-	return (color_Fitxa != contrari.color_Fitxa); //si son diferents
-}
-
 void Fitxa::netejaMovimentsValids() {
 	for (int i = 0; i < m_numMoviments; i++) {
 		m_movimentsValids[i] = Moviment();
