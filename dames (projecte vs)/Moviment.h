c8d3edf47ee5ad652321b5a20a67861a6fa99352
NIU1747579 <marta.tomey@autonoma.cat>
2025-06-13 22:58:22 +0200

    Update Moviment.h

--- dames (projecte vs)/Moviment.h
+++ dames (projecte vs)/Moviment.h
@@ -10,14 +10,21 @@ class Moviment
 {
 public:
 	Moviment(){}
-
+	Moviment(const string& origen, const string& desti);
 	Posicio getUltimaPosicio() const;
+
+	// Afegit
+	Posicio getPosicioOrigen() const;
+	Posicio getPosicioDesti() const;
+
 	void afegirCaptura(const Posicio& captura) { m_captures.push_back(captura); }
 	void afegirPosicio(const Posicio& pos) { m_posicions.push_back(pos); }
 	Posicio getPosicio(int index) const { return m_posicions[index]; }
 	int getNumPosicions() const;
 
 private:
+	string m_origen;
+	string m_desti;
 	vector<Posicio> m_posicions; 
 	vector<Posicio> m_captures;
 }; 
