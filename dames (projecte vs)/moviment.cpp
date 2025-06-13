c464347ce14b6b62316d61e5d802019dc1898306
NIU1747579 <marta.tomey@autonoma.cat>
2025-06-13 23:02:04 +0200

    Update moviment.cpp

--- dames (projecte vs)/moviment.cpp
+++ dames (projecte vs)/moviment.cpp
@@ -1,6 +1,5 @@
 #include "Moviment.h"
 
-
 Posicio Moviment::getUltimaPosicio() const {
     if (!m_posicions.empty()) 
         return m_posicions.back(); 
@@ -8,6 +7,24 @@ Posicio Moviment::getUltimaPosicio() const {
     return Posicio(); // Devuelve pos invalida si no hay posiciones
 }
 
+Moviment::Moviment(const string& origen, const string& desti)
+	: m_origen(origen), m_desti(desti) {
+	// Inicializa las posiciones con origen y destino
+	m_posicions.push_back(Posicio(origen));
+	m_posicions.push_back(Posicio(desti));
+}
 int Moviment::getNumPosicions() const {
     return (int)m_posicions.size();
+}
+
+Posicio Moviment::getPosicioOrigen() const {
+    if (m_posicions.size() > 0)
+        return m_posicions[0];
+    return Posicio();
+}
+
+Posicio Moviment::getPosicioDesti() const {
+    if (m_posicions.size() > 1)
+        return m_posicions[1];
+    return Posicio();
 }
