d87521425c37f3eb333b15995cc22167f7b89231
NIU1747579 <marta.tomey@autonoma.cat>
2025-06-13 23:03:55 +0200

    Update cuaMoviment.h

--- dames (projecte vs)/cuaMoviment.h
+++ dames (projecte vs)/cuaMoviment.h
@@ -5,14 +5,15 @@
 #include <string>
 #include <fstream>
 #include "info_joc.hpp"
-#include "Moviment.h"
 #include "posicio.hpp"
+#include "Moviment.h"
 
 using namespace std;
 
 struct NodeMoviment {
 	Moviment moviment; // Dades del moviment
 	NodeMoviment* seguent; // Punter al seguent node
+
 };
 
 class CuaMoviments {
@@ -20,17 +21,18 @@ public:
 	CuaMoviments() : m_primer(nullptr), m_ultim(nullptr) {}
 	~CuaMoviments() { neteja(); }
 
-	// Neteja tots el moviments de la cua
+	Moviment getUltim() const;
+	Moviment getPrimer() const;
+
 	void neteja();
 	// Afegeix un unic moviment al final de la cua
 	void afegeixMoviment(const string& origen, const string& desti);
 	Moviment treuMoviment();
 	bool esBuida() const { return m_primer == nullptr; }
 	void guardaMoviments(const string& nomFitxer) const;
-	void carregaMoviments(const string& nomFitxer);
 
 private:
-	NodeMoviment* m_primer;
+	NodeMoviment* m_primer; 
 	NodeMoviment* m_ultim;
 };
 
